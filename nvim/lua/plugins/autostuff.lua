4b886e8c832d9a812e30f3c300af656451f68a3a
maxvolume321 <ben@schonbeck.io>
2025-03-27 12:28:32 -0400

    Added auto pairs, tags

---
+++ nvim/lua/plugins/autostuff.lua
@@ -0,0 +1,13 @@
+return
+{
+    {
+        "windwp/nvim-ts-autotag"
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {
+            disable_filetype = {"TelescopePrompt", "vim"}
+        }
+    }
+}

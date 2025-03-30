f653380e89c9b5776f37734972686200bd26405c
maxvolume321 <ben@schonbeck.io>
2025-03-30 12:19:33 -0400

    Updated treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,24 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup {
-            ensure_installed = {
-                "c",
-                "cpp",
-                "lua",
-                "query",
-                "elixir",
-                "typescript",
-                "javascript",
-                "html",
-                "css",
-                "rust",
-                "python",
-                "go"
-            },
-            autotag = {
-                enable = false,
-            }
+    main = "nvim-treesitter.configs",
+    opts = {
+        ensure_installed = {
+            "c",
+            "cpp",
+            "lua",
+            "query",
+            "elixir",
+            "typescript",
+            "javascript",
+            "html",
+            "css",
+            "rust",
+            "python",
+            "go"
+        },
+        autotag = {
+            enable = false,
         }
-    end
+    }
 }
